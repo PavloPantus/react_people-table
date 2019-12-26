a4049b9ad354aa1c8cb2db313dad5cb18ea63f94
PavloPantus <mr.pantus@gmail.com>
2019-12-26 21:11:58 +0200

    Updates

---
+++ precache-manifest.6bedb303c1238b9ffe8faddd8de93a7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2179f26f956461b5dee92e6ee935a5c1",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "7849f5cc703dec5c0c6d",
+    "url": "/react_people-table/static/css/main.b587b7b2.chunk.css"
+  },
+  {
+    "revision": "6bcd8f597931b8065882",
+    "url": "/react_people-table/static/js/2.622bbb54.chunk.js"
+  },
+  {
+    "revision": "7849f5cc703dec5c0c6d",
+    "url": "/react_people-table/static/js/main.594b7acb.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
