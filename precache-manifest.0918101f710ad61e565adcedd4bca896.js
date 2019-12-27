07632c5f4d9bf20f089533eaadbab34f2e519898
PavloPantus <mr.pantus@gmail.com>
2019-12-27 22:12:29 +0200

    Updates

---
+++ precache-manifest.0918101f710ad61e565adcedd4bca896.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35873285494a536bde871d1badea3aa",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "c450424045d7e8a31612",
+    "url": "/react_people-table/static/css/main.cb32952f.chunk.css"
+  },
+  {
+    "revision": "b5db442531b848f59fd3",
+    "url": "/react_people-table/static/js/2.027f068e.chunk.js"
+  },
+  {
+    "revision": "c450424045d7e8a31612",
+    "url": "/react_people-table/static/js/main.c1b18cf0.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_people-table/precache-manifest.6bedb303c1238b9ffe8faddd8de93a7b.js"
+  "/react_people-table/precache-manifest.0918101f710ad61e565adcedd4bca896.js"
 );
 
 self.addEventListener('message', (event) => {
